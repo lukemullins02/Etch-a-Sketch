1ffbe57ca53d28cda279258fabaa909a1b7e7860
= <lukejmullins@gmail.com>
2025-08-15 10:08:44 -0400

    Add proper sizing

--- proj.js
+++ proj.js
@@ -1,18 +1,31 @@
 const container = document.querySelector(".container");
 
+// function numOfDivs(num) {
+//   for (let i = 0; i < num; i++) {
+//     let column = document.createElement("div");
+//     column.classList.add("column");
+//     for (let j = 0; j < num; j++) {
+//       let row = document.createElement("div");
+//       row.classList.add("row");
+//       row.style.border = "1px solid black";
+
+//       column.appendChild(row);
+//     }
+//     container.appendChild(column);
+//   }
+// }
+
 function numOfDivs(num) {
+  let size = container.clientWidth / num;
   for (let i = 0; i < num; i++) {
-    let column = document.createElement("div");
-    column.classList.add("column");
     for (let j = 0; j < num; j++) {
       let row = document.createElement("div");
       row.classList.add("row");
-      row.style.border = "2px solid black";
-      row.textContent = "hello";
-      column.appendChild(row);
+      row.style.width = `${size}px`;
+      row.style.height = `${size}px`;
+      container.appendChild(row);
     }
-    container.appendChild(column);
   }
 }
 
-numOfDivs(16);
+numOfDivs(100);
