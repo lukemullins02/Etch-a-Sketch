fdda8394b59ce92f6976cec259e1ae229735a454
= <lukejmullins@gmail.com>
2025-08-15 10:35:51 -0400

    Add button to change sizes

--- proj.js
+++ proj.js
@@ -1,19 +1,5 @@
 const container = document.querySelector(".container");
-
-// function numOfDivs(num) {
-//   for (let i = 0; i < num; i++) {
-//     let column = document.createElement("div");
-//     column.classList.add("column");
-//     for (let j = 0; j < num; j++) {
-//       let row = document.createElement("div");
-//       row.classList.add("row");
-//       row.style.border = "1px solid black";
-
-//       column.appendChild(row);
-//     }
-//     container.appendChild(column);
-//   }
-// }
+const btn = document.querySelector("#newGrid");
 
 function numOfDivs(num) {
   let size = container.clientWidth / num;
@@ -28,4 +14,28 @@ function numOfDivs(num) {
   }
 }
 
-numOfDivs(100);
+let def = 16;
+numOfDivs(def);
+
+function gridSize() {
+  let num = prompt("Please enter new grid size.");
+  num = Number(num);
+  if (Number.isNaN(num)) {
+    alert("Please enter a number.");
+  } else if (num > 100) {
+    alert("Please enter a number less than 100.");
+  } else if (num <= 0) {
+    alert("Please enter a number greater than 0.");
+  } else {
+    let child = container.lastElementChild;
+    while (child) {
+      container.removeChild(child);
+      child = container.lastElementChild;
+    }
+    numOfDivs(num);
+  }
+}
+
+btn.addEventListener("click", () => {
+  gridSize();
+});
